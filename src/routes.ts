bd7df3ab15d1ff158f16b14cc7166961ea7282cf
danilobrm <danilobr.web@gmail.com>
2023-05-16 18:20:37 -0300

    feature: user create account in the database

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import { CreateCustomerController } from './controllers/customer/createCustomerController';
+import { Validate } from './middlewares/validate';
 
 const router = Router();
+
+router.post(
+  '/register',
+  new Validate().validate,
+  new CreateCustomerController().create,
+);
 
 export { router };
