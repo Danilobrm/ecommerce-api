3be2681daa22502e8ef248211749d298e0a556c8
danilobrm <danilobr.web@gmail.com>
2023-05-18 19:02:48 -0300

    service to create on database is now generic and user can create account by this generic class

---
+++ src/controllers/customer/createCustomer.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import { ICreateCustomer } from '../../interfaces/customer';
+import { Repository } from '../../services/customer/repositoryService';
+import { ModelSelect } from '../../interfaces/repository';
+import { ICustomerData } from '../../interfaces/customer';
+import { hash } from 'bcryptjs';
+
+export class CreateCustomer implements ICreateCustomer {
+  async create(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+    const createCustomerService = new Repository();
+
+    const customerData: ICustomerData = {
+      name,
+      email,
+      password: await hash(password, 8),
+    };
+
+    const select: ModelSelect['customer'] = {
+      id: true,
+      name: true,
+      email: true,
+    };
+
+    const customer = await createCustomerService.create(
+      'customer',
+      customerData,
+      select,
+    );
+
+    return res.json(customer);
+  }
+}
