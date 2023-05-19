44c5c3b250c5e3c4b90070b9b0cfba49268451dd
danilobrm <danilobr.web@gmail.com>
2023-05-19 17:33:40 -0300

    customer implementing its own repository to create user

--- src/controllers/customer/createCustomer.ts
+++ src/controllers/customer/createCustomer.ts
@@ -1,32 +1,27 @@
 import { Request, Response } from 'express';
 import { ICreateCustomer } from '../../interfaces/customer';
-import { Repository } from '../../services/repository';
-import { ModelSelect } from '../../interfaces/repository';
-import { ICustomerData } from '../../interfaces/customer';
+import { Repository } from '../../services/customer/repository';
 import { hash } from 'bcryptjs';
+import { Prisma } from '@prisma/client';
 
 export class CreateCustomer implements ICreateCustomer {
   async create(req: Request, res: Response): Promise<Response> {
     const { name, email, password } = req.body;
     const createCustomerService = new Repository();
 
-    const customerData: ICustomerData = {
+    const customerData: Prisma.CustomerCreateInput = {
       name,
       email,
       password: await hash(password, 8),
     };
 
-    const select: ModelSelect['customer'] = {
+    const select: Prisma.CustomerSelect = {
       id: true,
       name: true,
       email: true,
     };
 
-    const customer = await createCustomerService.create(
-      'customer',
-      customerData,
-      select,
-    );
+    const customer = await createCustomerService.create(customerData, select);
 
     return res.json(customer);
   }
