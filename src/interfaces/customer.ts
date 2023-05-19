02fdf279c9682e762096e44e9b742f473a569924
danilobrm <danilobr.web@gmail.com>
2023-05-19 17:33:40 -0300

    customer implementing its own repository to create user

--- src/interfaces/customer.ts
+++ src/interfaces/customer.ts
@@ -1,17 +1,19 @@
+import { Prisma } from '@prisma/client';
 import { Request, Response } from 'express';
 
 export interface ICreateCustomer {
   create(req: Request, res: Response): Promise<Response>;
 }
 
-export interface ICustomerData {
-  name: string;
-  email: string;
-  password: string;
-}
-
 export interface ICustomerSelectedData {
   id: number;
   name: string;
   email: string;
+}
+
+export interface ICustomerRepository {
+  create(
+    data: Prisma.CustomerCreateInput,
+    select: Prisma.CustomerSelect,
+  ): Promise<ICustomerSelectedData>;
 }
