d5dd2f7ddab4207edc16676c1c4661a6c852be19
danilobrm <danilobr.web@gmail.com>
2023-05-30 17:40:07 -0300

    change of id type from number to string in database

--- prisma/migrations/20230530185912_user_table/migration.sql
+++ prisma/migrations/20230530185912_user_table/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE `customers` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE `customers` (
 
 -- CreateTable
 CREATE TABLE `addresses` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `street` VARCHAR(191) NOT NULL,
     `number` VARCHAR(191) NOT NULL,
     `complement` VARCHAR(191) NULL,
@@ -20,7 +20,7 @@ CREATE TABLE `addresses` (
     `city` VARCHAR(191) NOT NULL,
     `state` VARCHAR(191) NOT NULL,
     `postal_code` VARCHAR(191) NOT NULL,
-    `customer_id` INTEGER NOT NULL,
+    `customer_id` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
