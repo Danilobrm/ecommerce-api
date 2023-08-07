7e734e5ea7a4941ebc5443744271acd3d230a6b2
danilobrm <danilobr.web@gmail.com>
2023-08-07 18:22:03 -0300

    ref: listCategory code refactored

--- src/services/category/ListCategoryService.ts
+++ src/services/category/ListCategoryService.ts
@@ -2,12 +2,7 @@ import prismaClient from '../../prisma';
 
 class ListCategoryService {
   async execute() {
-    const category = await prismaClient.category.findMany({
-      select: {
-        id: true,
-        name: true,
-      },
-    });
+    const category = await prismaClient.category.findMany({ select: { id: true, name: true } });
 
     return category;
   }
