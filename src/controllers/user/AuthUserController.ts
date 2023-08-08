74276bb5a4928c3df5a62d4205f646a5a5005a47
DANILO DE BRITO RODRIGUES <danilo.brodrigues@a.ucb.br>
2023-08-08 09:36:19 -0300

    ref: user authentication code refactored

--- src/controllers/user/AuthUserController.ts
+++ src/controllers/user/AuthUserController.ts
@@ -7,7 +7,6 @@ export class AuthUserController implements AuthenticateRequest {
     const { email, password } = req.body;
 
     const authUserService = new AuthUserService();
-
     const auth = await authUserService.execute({ email, password });
 
     return res.json(auth);
