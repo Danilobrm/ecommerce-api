d1cfca35a1a323a447a128d244907cde051a8dc4
danilobrm <danilobr.web@gmail.com>
2023-08-10 20:03:58 -0300

    ref: createProduct endpoint codes and test refactored

--- src/services/product/CreateProductService.ts
+++ src/services/product/CreateProductService.ts
@@ -1,15 +1,9 @@
+import { Product } from '@prisma/client';
+import { CreateRequestService } from '../../interfaces/services';
 import prismaClient from '../../prisma';
 
-interface ProductRequest {
-  name: string;
-  price: string;
-  description: string;
-  banner: string;
-  category_id: string;
-}
-
-class CreateProductService {
-  async execute({ name, price, description, banner, category_id }: ProductRequest) {
+class CreateProductService implements CreateRequestService<Product> {
+  async create({ name, price, description, banner, category_id }: Product): Promise<Product> {
     const product = await prismaClient.product.create({ data: { name, price, description, banner, category_id } });
 
     return product;
