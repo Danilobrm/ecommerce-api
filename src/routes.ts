183fa988c337b339a0e96cca06abcf92ef5049bf
danilobrm <danilobr.web@gmail.com>
2023-08-10 20:26:47 -0300

    ref: changing some endpoints names

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,7 @@ import uploadConfig from './config/multer';
 import { Router } from 'express';
 
 import { ValidateUserRegister } from './middlewares/crud/validation/validateRegister';
-import { ValidateUserAuth } from './middlewares/auth/validation/validateAuth';
-import { ValidateCategory } from './middlewares/crud/validation/validateCategory';
+import { ValidateUserAuthentication } from './middlewares/auth/validation/validateUserAuthentication';
 import { isAuthenticated } from './middlewares/auth/isAuthenticated';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
@@ -15,28 +14,29 @@ import { CreateProductController } from './controllers/product/CreateProductCont
 import { AddItemController } from './controllers/cart/AddItemController';
 
 import { AuthUserController } from './controllers/user/AuthUserController';
-import { CreateCart, DeleteCart } from './middlewares/crud/validation/validateCart';
+import { CheckUserCart } from './middlewares/crud/validation/CheckUserCart';
 import { RemoveItemController } from './controllers/cart/RemoveItemController';
 
 import { DetailCartController } from './controllers/cart/DetailCartController';
+import { DeleteCartController } from './controllers/cart/DeleteCartController';
 
 const router = Router();
 const upload = multer(uploadConfig.upload(`./tmp`));
 
 // auth routes
 router.post('/register', new ValidateUserRegister().validate, new CreateUserController().create);
-router.post('/login', new ValidateUserAuth().validate, new AuthUserController().authenticate);
+router.post('/login', new ValidateUserAuthentication().validate, new AuthUserController().authenticate);
 router.post('/address', isAuthenticated, new CreateAddressController().create);
 
 // category routes
-router.post('/category', isAuthenticated, new ValidateCategory().validate, new CreateCategoryController().create);
+router.post('/category', isAuthenticated, new CreateCategoryController().create);
 
 // product routes
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().create);
 
 // cart items routes
-router.post('/cart/add', isAuthenticated, new CreateCart().validate, new AddItemController().add);
-router.delete('/cart/remove', isAuthenticated, new RemoveItemController().remove, new DeleteCart().validate);
+router.post('/cart/add', isAuthenticated, new CheckUserCart().check, new AddItemController().add);
+router.delete('/cart/remove', isAuthenticated, new RemoveItemController().remove, new DeleteCartController().delete);
 router.get('/cart/detail', isAuthenticated, new DetailCartController().read);
 
 export { router };
