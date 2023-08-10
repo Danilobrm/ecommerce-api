52f71c7a60d935e019b8ac3aead292a402cbe523
danilobrm <danilobr.web@gmail.com>
2023-08-10 19:57:44 -0300

    ref: create caregory codes refactored

--- src/tests/endpoints/category/crud/createCategory.spec.ts
+++ src/tests/endpoints/category/crud/createCategory.spec.ts
@@ -2,11 +2,60 @@ import { Request, Response } from 'express';
 import { CreateRequest } from '../../../../interfaces/controllers/CRUD';
 import prismaClient from '../../../../prisma';
 import { prismaMock } from '../../../../singleton';
+import { Category } from '@prisma/client';
+
+interface Dependencies {
+  mockRequest: Request;
+  mockResponse: Response;
+}
+
+describe('create category test', () => {
+  const _dependencies: Dependencies = {
+    mockRequest: {
+      body: { name: 'roupas' },
+    } as unknown as Request,
+    mockResponse: {
+      status: jest.fn((status: number) => status),
+      json: jest.fn((response: Category) => JSON.stringify(response)),
+    } as unknown as Response,
+  };
+
+  it('should be called once', async () => {
+    const sut = new MockCreateCategoryController();
+    const spy = jest.spyOn(sut, 'create');
+    await sut.create(_dependencies.mockRequest, _dependencies.mockResponse);
+
+    expect(spy).toBeCalledTimes(1);
+  });
+
+  it('should create category in database and return its json', async () => {
+    const sut = new MockCreateCategoryController();
+    const mockResolvedValue = { id: 'id', name: 'roupas', created_at: mockDate, updated_at: mockDate };
+    prismaMock.category.create.mockResolvedValue(mockResolvedValue);
+
+    await expect(sut.create(_dependencies.mockRequest, _dependencies.mockResponse)).resolves.toEqual(
+      JSON.stringify(mockResolvedValue),
+    );
+  });
+
+  it('should give error if no category name was provided', async () => {
+    _dependencies.mockRequest.body.name = '';
+
+    const sut = new MockCreateCategoryController();
+    const category = await sut.create(_dependencies.mockRequest, _dependencies.mockResponse);
+
+    expect(category).toEqual(JSON.stringify({ status: 400, response: 'Categoria precisa de um nome!' }));
+  });
+});
 
 class MockCreateCategoryController implements CreateRequest {
   async create(req: Request, res: Response) {
     const { name } = req.body;
-    if (!name) res.status(400).json('Categoria precisa de um nome!');
+    if (!name)
+      return res.json({
+        status: 400,
+        response: 'Categoria precisa de um nome!',
+      });
 
     const createCategoryService = new MockCreateCategoryService();
     const category = await createCategoryService.execute({ name });
@@ -26,45 +75,5 @@ class MockCreateCategoryService {
     return category;
   }
 }
-
-const createSut = () => {
-  return new MockCreateCategoryController();
-};
-
-describe('test create category controller', () => {
-  const sut = createSut();
-  it('should be called once', () => {
-    const spy = jest.spyOn(sut, 'create');
-
-    sut.create(mockRequest, mockResponse);
-
-    expect(spy).toBeCalledTimes(1);
-  });
-
-  it('should return category json', async () => {
-    prismaMock.category.create.mockResolvedValue({
-      id: 'id',
-      name: 'roupas',
-      created_at: mockDate,
-      updated_at: mockDate,
-    });
-
-    await expect(sut.create(mockRequest, mockResponse)).resolves.toEqual(
-      JSON.stringify({
-        id: 'id',
-        name: 'roupas',
-        created_at: mockDate,
-        updated_at: mockDate,
-      }),
-    );
-  });
-});
-
-const mockRequest = {
-  body: { name: 'roupas' },
-} as unknown as Request;
-
-const mockResponse = {} as unknown as Response;
-mockResponse.json = (data: Request): Response => JSON.stringify(data) as unknown as Response;
 
 export const mockDate = new Date() as Date;
