354feba9efa32defa993b8d62c25c537dc5f2c8a
DANILO DE BRITO RODRIGUES <danilo.brodrigues@a.ucb.br>
2023-08-11 11:27:13 -0300

    ref: cart tests codes refactored to return test coverage

--- src/tests/endpoints/cart/crud/deleteCart.spec.ts
+++ src/tests/endpoints/cart/crud/deleteCart.spec.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from 'express';
 import { Cart } from '@prisma/client';
-import { DeleteService } from '../../../../interfaces/services';
-import prismaClient from '../../../../prisma';
 import { prismaMock } from '../../../../singleton';
+import { DeleteCartController } from '../../../../controllers/cart/DeleteCartController';
 
 interface Dependencies {
   mockRequest: Request;
@@ -10,19 +9,8 @@ interface Dependencies {
 }
 
 describe('delete user cart test', () => {
-  const _dependencies: Dependencies = {
-    mockRequest: {
-      query: { cart_id: 'cartid' },
-      user_id: 'userid',
-    } as unknown as Request,
-    mockResponse: {
-      status: jest.fn((status: number) => status),
-      json: jest.fn((response: Cart) => JSON.stringify(response)),
-    } as unknown as Response,
-  };
-
   it('should be called once', async () => {
-    const sut = new MockDeleteCartController();
+    const sut = new DeleteCartController();
     const spy = jest.spyOn(sut, 'delete');
     await sut.delete(_dependencies.mockRequest, _dependencies.mockResponse);
 
@@ -38,32 +26,20 @@ describe('delete user cart test', () => {
     } as Cart;
 
     prismaMock.cart.delete.mockResolvedValue(mockResolvedValue);
-    const sut = new MockDeleteCartController();
+    const sut = new DeleteCartController();
     const cart = await sut.delete(_dependencies.mockRequest, _dependencies.mockResponse);
 
     expect(cart).toEqual(JSON.stringify(mockResolvedValue));
   });
 });
 
-export class MockDeleteCartController {
-  async delete(req: Request, res: Response) {
-    const cart_id = req.cart_id;
-
-    const removeCartService = new MockDeleteCartService();
-    const cart = await removeCartService.delete({ cart_id });
-
-    return res.json(cart);
-  }
-}
-
-interface RequestData {
-  cart_id: string;
-}
-
-class MockDeleteCartService implements DeleteService<RequestData, Cart> {
-  async delete({ cart_id }: RequestData): Promise<Cart> {
-    const cart = await prismaClient.cart.delete({ where: { id: cart_id } });
-
-    return cart;
-  }
-}
+const _dependencies: Dependencies = {
+  mockRequest: {
+    query: { cart_id: 'cartid' },
+    user_id: 'userid',
+  } as unknown as Request,
+  mockResponse: {
+    status: jest.fn((status: number) => status),
+    json: jest.fn((response: Cart) => JSON.stringify(response)),
+  } as unknown as Response,
+};
